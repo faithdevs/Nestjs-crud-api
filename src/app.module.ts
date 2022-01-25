bfa7d756613dd9a5131e4bd873d0797f3bf1d44e
faithdevs <ugbeshefaith@gmail.com>
2022-01-25 09:50:54 +0100

    added images

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MessagesModule } from './messages/messages.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [UsersModule, MessagesModule, TransactionsModule],
+  imports: [UsersModule, MessagesModule, TransactionsModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
